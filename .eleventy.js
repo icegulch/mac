47ee5017d66509b2bda9745a7b5f472796fe604b
Steven Bailey <stevenbailey@Stevens-MacBook-Pro.local>
2021-05-16 21:20:27 -0600

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,96 @@
+const yaml = require("js-yaml");
+const htmlmin = require("html-minifier");
+const { DateTime } = require("luxon");
+const _ = require("lodash");
+const markdownFilter = require('./src/filters/markdown-filter.js');
+const slugify = require("slugify");
+require("dotenv").config();
+
+module.exports = function (eleventyConfig) {
+
+  // A useful way to reference the context we are runing eleventy in
+  let env = process.env.ELEVENTY_ENV;
+
+  // Let some files pass through to public
+  eleventyConfig.addPassthroughCopy("./src/robots.txt");
+  eleventyConfig.addPassthroughCopy("./src/images/**");
+  eleventyConfig.addPassthroughCopy("./src/mac-directions-with-profile.pdf");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/css");
+
+  // Support YML because JSON effs up dates
+  eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+
+  // Markdown, please
+  eleventyConfig.addFilter('markdownFilter', markdownFilter);
+
+  // Slugify
+  eleventyConfig.addFilter("slugify", function (str) {
+    return slugify(str, {
+      lower: true,
+      replacement: "-",
+      remove: /[*+~.·,()'"`´%!?¿:@]/g
+    });
+  });
+
+  // Debug
+  eleventyConfig.addFilter("debugger", (...args) => {
+    console.log(...args)
+    debugger;
+  })
+
+  // Year only
+  eleventyConfig.addFilter("gimmeYear", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("yyyy");
+  });
+
+  // Nice date for post-y type things
+  eleventyConfig.addFilter("postDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLLL d, yyyy");
+  });
+
+  // Short date for results page
+  eleventyConfig.addFilter("shortDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLL d");
+  });
+
+  // htmlDateString
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
+  // Helper to sort pages collection by frontmatter field "order"
+  eleventyConfig.addCollection("orderedPages", function (collection) {
+    return collection.getFilteredByTag("pages").sort((a, b) => {
+      return a.data.order - b.data.order;
+    });
+  });
+
+  // Minify HTML Output
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if(process.env.ELEVENTY_ENV === 'production' && outputPath && outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  // deep merge
+  eleventyConfig.setDataDeepMerge(true);
+
+  return {
+    addPassthroughCopy: true,
+    templateFormats: ['md', 'njk', 'html'],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
---
+++ src/posts/2014-06-10-bersvendsen.md
@@ -0,0 +1,15 @@
+---
+date: 2014-06-10
+title: Odd Bersvendsen's Mount Adams Challenge
+related_results: ['']
+---
+
+<p>Thanks for the challenge, it made me go for an adventure. I might have taken a wrong turn somewhere since my distance and total elevation gain where higher than the 5.3 mi and 4488 in the invite?</p>
+<p>Here is the link to my trace.</p>
+<p>Loved every step… even coming down and celebrating Independence Day.</p>
+<p>Thanks for the beverage! I might try to improve my time later in the season and let you know.</p>
+<p>Odd Bersvendsen
+Top Notch Inn/MWVMHV</p>
+<img src="/images/uploads/oddstrava.png" alt="Screenshot of Odd's Strava" width="423" height="750" class="img-fluid">
+<img src="/images/uploads/tumblrinlinepkpe2hdmg11si9ly8500.jpg" alt="Odd at the Summit Sign" width="500" height="375" class="img-fluid">
+
